e2c213dcaed63bcb07288dcc5e3d27bd477c66ca
Michael KAPPAURER <kami@fhv.at>
2021-01-19 17:54:57 +0100

    Decision Matrix added and sub process edited

---
+++ src/test/java/com/fhv/master/BusinessLogic/test/SetupLoanAgreementTest.java
@@ -0,0 +1,69 @@
+package com.fhv.master.BusinessLogic.test;
+
+import com.fhv.master.BusinessLogic.SetupLoanAgreement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetupLoanAgreementTest {
+    SetupLoanAgreement setupLoanAgreement = new SetupLoanAgreement();
+
+    @Test
+    void execute() {
+
+    }
+
+    @Test
+    void setupLoanAgreementTask() throws FileNotFoundException {
+        insertData();
+        setupFile();
+        getLoanData();
+        getCustomerData();
+    }
+
+    @Test
+    void insertData() throws FileNotFoundException {
+        setupLoanAgreement.InsertData("Hans Haus","Auto");
+        Path path = Paths.get("C:/Users/kami/OneDrive - FH Vorarlberg/Master/sem1/Software Lebenszyklus und Qualität/Sieber/BankCreditProcess/LoanAgreement.txt");
+        StringBuilder stb = new StringBuilder();
+        try(Stream<String> stream = Files.lines(path)) {
+            stream.forEach(s -> stb.append(s).append("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(stb.toString().contains("Hans Haus"));
+        assertTrue(stb.toString().contains("Auto"));
+
+    }
+
+    @Test
+    void setupFile() {
+        setupLoanAgreement.SetupFile();
+        File file = new File("C:/Users/kami/OneDrive - FH Vorarlberg/Master/sem1/Software Lebenszyklus und Qualität/Sieber/BankCreditProcess/LoanAgreement.txt");
+        assertTrue(file.exists());
+        assertTrue(file.isFile());
+    }
+
+    @Test
+    void getLoanData() {
+        String loanData = setupLoanAgreement.GetLoanData();
+        assertNotSame("", loanData);
+    }
+
+    @Test
+    void getCustomerData() {
+        String cusomerData = setupLoanAgreement.GetCustomerData();
+        assertNotSame("", cusomerData);
+    }
+}
