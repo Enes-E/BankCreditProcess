4357ca9427eca783473032ce5aa92e9390c7c950
Michael KAPPAURER <kami@fhv.at>
2021-01-26 17:08:26 +0100

    Process finished and running

--- src/main/java/com/fhv/master/BusinessLogic/SetupLoanAgreement.java
+++ src/main/java/com/fhv/master/BusinessLogic/SetupLoanAgreement.java
@@ -14,19 +14,19 @@ import java.util.logging.Logger;
 public class SetupLoanAgreement implements JavaDelegate {
 
     private final Logger LOGGER = Logger.getLogger(SetupLoanAgreement.class.getName());
-    private int loanAmount = 0;
+    private double loanAmount = 0;
     private String customerFirstName = "";
     private String customerLastName = "";
     private String loanPurpose = "";
-    private String cashier = "";
+    private String decisionMaker = "";
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        loanAmount = (Integer) delegateExecution.getVariable("loanAmount");
+        loanAmount = (Double) delegateExecution.getVariable("loanAmount");
         customerFirstName = (String) delegateExecution.getVariable("customerFirstName");
         customerLastName = (String) delegateExecution.getVariable("customerLastName");
         loanPurpose = (String) delegateExecution.getVariable("loanPurpose");
-        cashier = (String) delegateExecution.getVariable("cashier");
+        decisionMaker = (String) delegateExecution.getVariable("decisionMaker");
         execute();
     }
 
@@ -44,7 +44,7 @@ public class SetupLoanAgreement implements JavaDelegate {
     public boolean SetupLoanAgreementTask() {
         try{
             SetupFile();
-            CreateAgreement(customerFirstName,customerLastName, loanAmount, loanPurpose, cashier);
+            CreateAgreement(customerFirstName, customerLastName, loanAmount, loanPurpose, decisionMaker);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -66,8 +66,8 @@ public class SetupLoanAgreement implements JavaDelegate {
         }
     }
 
-    public void CreateAgreement(String customerFirstName, String customerLastName, int loanAmount, String loanPurpose,
-                                String cashier) {
+    public void CreateAgreement(String customerFirstName, String customerLastName, double loanAmount, String loanPurpose,
+                                String decisionMaker) {
 
         StringBuilder stb = new StringBuilder();
         stb.append("Loan aggrement for: ");
@@ -77,10 +77,10 @@ public class SetupLoanAgreement implements JavaDelegate {
         stb.append("\n With the following data: ");
         stb.append("\n Purpose: ");
         stb.append(loanPurpose);
-        stb.append("\n Amount:");
+        stb.append("\n Amount: ");
         stb.append(loanAmount);
         stb.append("\n Signed by: ");
-        stb.append(cashier);
+        stb.append(decisionMaker);
 
         try {
             Files.write(Paths.get("LoanAgreement.txt"), stb.toString().getBytes(), StandardOpenOption.WRITE);
@@ -104,7 +104,7 @@ public class SetupLoanAgreement implements JavaDelegate {
             stb.append("\n Es folgen die Vertragsdaten für die Kreditaufnahme: ");
             stb.append(getAgreementContent());
             stb.append("mit freundlichen Grüßen, ");
-            stb.append(cashier);
+            stb.append(decisionMaker);
 
             LOGGER.info("Agreement Sent");
         } catch (Exception e) {
